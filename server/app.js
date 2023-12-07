3731febae4a31355c1ebb7ef7b88a8cbaac7f7ad
k3y0n <y3.0n@yandex.by>
2023-12-07 12:26:58 +0300

    connect cloud mongodb

--- server/app.js
+++ server/app.js
@@ -1,10 +1,34 @@
-import express from 'express'
-import chalk from "chalk"
-import config from 'config'
+import express from "express";
+import chalk from "chalk";
+import config from "config";
+import mongoose from "mongoose";
 
-const app = express()
-const PORT = config.get('PORT') ?? 808;
+const app = express();
+const PORT = config.get("PORT") ?? 8080;
 
-app.listen(PORT,()=>{
-    console.log(chalk.green(`Server started on  port:${PORT}`))
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+if (process.env.NODE_ENV === "production") {
+  console.log("start prod mode");
+} else {
+  console.log("start dev mode");
+}
+
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get("mongodb"));
+    console.log(chalk.green(`Mongodb connected 💻`));
+
+
+    app.listen(PORT, () => {
+      console.log(chalk.green(`Server started on  port ⚡️:${PORT}`));
+    });
+  } catch (error) {
+    console.error(chalk.red(`${error.message}`));
+    process.exit(1);
+  }
+};
+
+start();
